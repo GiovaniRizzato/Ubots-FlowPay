be082e6e55a7b53cdf1205f01a070dc48c011e9b
Giovani Rizzato <giovanirizzato@gmail.com>
2024-11-26 11:57:35 -0300

    Inicializando o projeto Springboot

---
+++ src/main/java/br/com/ubots/flowplay/FlowPlayApplication.java
@@ -0,0 +1,13 @@
+package br.com.ubots.flowplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowPlayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ubots/flowplay/FlowPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ubots.flowplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
